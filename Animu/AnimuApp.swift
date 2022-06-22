17fdbc81ee2f853a9478a70ded96dee9c177df19
Timmy Nguyen <timmypass99@gmail.com>
2022-06-21 18:22:23 -0700

    Initial Commit

---
+++ Animu/AnimuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimuApp.swift
+//  Animu
+//
+//  Created by Timmy Nguyen on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
